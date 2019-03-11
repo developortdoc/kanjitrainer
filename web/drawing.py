174fbad614d4a0f9bf2d6577a10aa472e8278452
developortdoc <brotrosen@gmail.com>
2019-03-11 21:19:48 +0900

    Recerence area added

--- web/drawing.py
+++ web/drawing.py
@@ -3,5 +3,5 @@ from flask import (Blueprint, render_template, request)
 bp = Blueprint('draw', __name__)
 
 @bp.route('/', methods=['GET', 'POST'])
-def canvas():
-    return render_template('canvas.html')
+def canvas(kanji="愛"):
+    return render_template('canvas.html',kanji=kanji)
