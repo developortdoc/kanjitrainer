e06440a3bdeb58c538fe6bd82a9a9dea5639f1c8
developortdoc <brotrosen@gmail.com>
2019-03-11 20:05:28 +0900

    Basic drawing system built

--- web/static/draw.js
+++ web/static/draw.js
@@ -1,8 +1,39 @@
+ctx=$('#drawing')[0].getContext('2d');
+ctx.lineWidth=20;
+var nowx=0;
+var nowy=0;
+var active=false;
+
 $('#drawing').mousedown(
   function(e){
-      var getx=e.pageX-this.offsetLeft;
-      var gety=e.pageY-this.offsetTop;
-
-      $('#test').html(getx+","+gety);
+    nowx=e.pageX-this.offsetLeft;
+    nowy=e.pageY-this.offsetTop;
+    ctx.moveTo(nowx, nowy);
+    active=true;
   }  
 );
+
+$('#drawing').mousemove(
+    function(e){
+        if(active){
+            var getx=e.pageX-this.offsetLeft;
+            var gety=e.pageY-this.offsetTop;
+            drawline(getx, gety);
+            nowx=getx;
+            nowy=gety;
+        }   
+    }  
+  );
+
+ $('#drawing').mouseup(
+    function(e){
+        active=false;
+      } 
+ )
+
+  function drawline(x,y){
+        ctx.lineTo(x,y);
+        ctx.stroke();
+  }
+
+
