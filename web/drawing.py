e7bd11685c9df9a8beb012e5bf89caed1a7d2656
developortdoc <brotrosen@gmail.com>
2019-03-15 15:17:07 +0900

    Layout improved

--- web/drawing.py
+++ web/drawing.py
@@ -1,7 +1,14 @@
-from flask import (Blueprint, render_template, request)
+from flask import (Blueprint, render_template, redirect, request, url_for)
 
 bp = Blueprint('draw', __name__)
 
 @bp.route('/', methods=['GET', 'POST'])
 def canvas(kanji="愛"):
+    if request.method == 'POST':
+        kanjitext = request.form['kanjitext']
+        kanji=kanjitext[:1]
     return render_template('canvas.html',kanji=kanji)
+
+@bp.route('/save', methods=['POST'])
+def save(kanji):
+    return redirect(url_for('drawing.canvas', kanji=kanji))
--- web/static/draw.js
+++ web/static/draw.js
@@ -1,19 +1,27 @@
-ctx=$('#drawing')[0].getContext('2d');
+cvs=$('#drawing');
+ctx=cvs[0].getContext('2d');
+ctx.fillStyle="#F0FFF0";
+ctx.fillRect(0, 0, cvs[0].width, cvs[0].height);
 ctx.lineWidth=20;
+
 var nowx=0;
 var nowy=0;
 var active=false;
+var points=[];
+var jsonstr="";
 
-$('#drawing').mousedown(
+
+cvs.mousedown(
   function(e){
     nowx=e.pageX-this.offsetLeft;
     nowy=e.pageY-this.offsetTop;
     ctx.moveTo(nowx, nowy);
+    register();
     active=true;
   }  
 );
 
-$('#drawing').mousemove(
+cvs.mousemove(
     function(e){
         if(active){
             var getx=e.pageX-this.offsetLeft;
@@ -21,19 +29,25 @@ $('#drawing').mousemove(
             drawline(getx, gety);
             nowx=getx;
             nowy=gety;
+            register();
         }   
     }  
   );
 
- $('#drawing').mouseup(
+ cvs.mouseup(
     function(e){
         active=false;
+        $("#data_json").val(JSON.stringify(points));
       } 
  )
 
-  function drawline(x,y){
+function drawline(x,y){
         ctx.lineTo(x,y);
         ctx.stroke();
-  }
+}
 
+function register(){
+    nowxy={x:nowx, y:nowy};
+    points.push(nowxy);
+}
 
